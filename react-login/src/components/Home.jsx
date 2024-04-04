3416a73ec07a6f01ef7fb943dc57256b20f9945f
wachirastudeo <wachirastudeo@gmail.com>
2024-04-04 20:32:03 +0700

    create ui login

---
+++ react-login/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import "../App.css";
+
+function Home() {
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default Home;
